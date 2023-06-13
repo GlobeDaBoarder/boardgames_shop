08f967afcf8051c09146789bf88f4b0dcc95241b
GlobeDaBoarder <glebivashyn@gmail.com>
2023-06-13 10:12:53 +0300

    rename dockerfile

--- start-mysql.sh
+++ start-mysql.sh
@@ -2,6 +2,7 @@
 
 # Start MySQL
 service mysql start
+echo "Waiting for MySQL to start..."
 sleep 10
 # Setup MySQL
 mysql -u root -p"root" -e "CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'; FLUSH PRIVILEGES;"
