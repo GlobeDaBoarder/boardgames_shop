df88132710b7441fd24457f95e76436856199559
GlobeDaBoarder <glebivashyn@gmail.com>
2023-08-01 13:41:58 +0300

    repository import cleanup

--- src/main/java/ua/rivnegray/boardgames_shop/repository/AddressRepository.java
+++ src/main/java/ua/rivnegray/boardgames_shop/repository/AddressRepository.java
@@ -3,7 +3,5 @@ package ua.rivnegray.boardgames_shop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.rivnegray.boardgames_shop.model.Address;
 
-import java.util.Optional;
-
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
